c193808f28f4b56e27f0de3a805faad692983dc2
Peter / Cobertos <me+git@cobertos.com>
2019-06-13 02:19:24 -0400

    MicrophoneSource GUI is super robust now

--- Assets/Scripts/VisualsTools/Editor/MicrophoneAudioSourceEditor.cs
+++ Assets/Scripts/VisualsTools/Editor/MicrophoneAudioSourceEditor.cs
@@ -13,35 +13,78 @@ namespace CobVisuals {
 [CanEditMultipleObjects]
 public class MicrophoneAudioSourceEditor : Editor {
     Label menuLabel;
+    Label statusLabel;
+
+    public MicrophoneAudioSource editorTarget {
+        get {
+            try {
+                return target as MicrophoneAudioSource;
+            }
+            catch(System.Exception e) {
+                Debug.Log("Not ready");
+                return null;
+            }
+        }
+    }
 
     public override VisualElement CreateInspectorGUI() {
         VisualElement customInspector = new VisualElement();
+        VisualElement selectorContainer = new VisualElement(){
+            style = {
+                flexDirection = FlexDirection.Row,
+                alignItems = Align.Stretch,
+                justifyContent = Justify.FlexStart
+            }
+        };
+        //Status text
+        statusLabel = new Label(){
+            text = "?",
+            style = {
+                width = 20 //Constant width to stop the selector from moving
+            }
+        };
+        selectorContainer.Add(statusLabel);
+        customInspector.Add(selectorContainer);
+        //Dropdown selector
         ToolbarMenu menu = new ToolbarMenu();
+        menu.menu.AppendAction("Off", OnSourceSelect, (a) => DropdownMenuAction.Status.Normal, null);
         menu.menu.AppendAction("Default Device", OnSourceSelect, (a) => DropdownMenuAction.Status.Normal, null);
         foreach(string deviceName in Microphone.devices) {
             menu.menu.AppendAction(deviceName, OnSourceSelect, (a) => DropdownMenuAction.Status.Normal, null);
         }
         menuLabel = new Label(){
-            text = "Default Device"
+            text = "---"
         };
         menu.Add(menuLabel);
-        customInspector.Add(menu);
+        selectorContainer.Add(menu);
+
+        Refresh();
+        editorTarget.DeviceStateChanged += Refresh; //Refresh when the device changes
         return customInspector;
     }
 
-    public void OnSourceSelect(DropdownMenuAction ev) {
-
-        MicrophoneAudioSource microphoneSource;
-        try {
-            microphoneSource = target as MicrophoneAudioSource;
-        }
-        catch(System.Exception e) {
-            Debug.Log("Not ready");
+    public void Refresh() {
+        if(editorTarget == null) {
             return;
         }
 
-        microphoneSource.deviceName = ev.name == "Default Device" ? null : ev.name;
-        menuLabel.text = ev.name;
+        menuLabel.text = editorTarget.deviceName ?? "Default Device";
+        statusLabel.text = (editorTarget.enabled && editorTarget.isListening) ? "✓" : "X";
+    }
+
+    public void OnSourceSelect(DropdownMenuAction ev) {
+        if(editorTarget == null) {
+            return;
+        }
+
+        if(ev.name == "Off") {
+            editorTarget.enabled = false;
+        }
+        else {
+            editorTarget.deviceName = ev.name == "Default Device" ? null : ev.name;
+            editorTarget.enabled = true;
+        }
+        Refresh();
     }
 }
 
--- Assets/Scripts/VisualsTools/MicrophoneAudioSource.cs
+++ Assets/Scripts/VisualsTools/MicrophoneAudioSource.cs
@@ -7,9 +7,10 @@ using TimeSpan = System.TimeSpan;
 namespace CobVisuals {
 
 /// <summary>
-/// Attaches the microphone to the current audio source
+/// Attaches a Microphone to the AudioSource on this GameObject
 /// </summary>
 /// <remarks>
+/// Enabling and disabling will teardown and setup any active microphone
 /// Unity is really weird with Microphone input so you might experience some weird garbage.
 /// - Latency seems to directly correlate to the length of the Microphone.Start() clip. Because it's an
 /// int, the lowest you can go is 1, and it will be 1 second of latency... :/
@@ -21,26 +22,30 @@ namespace CobVisuals {
 /// </remarks>
 [RequireComponent(typeof(AudioSource))]
 public class MicrophoneAudioSource : MonoBehaviour {
+    [SerializeField]
     private string _deviceName;
     public string deviceName {
         get {
             return _deviceName;
         }
         set {
-            if(Application.isPlaying) { //If playing, actually open the mic source
-                TeardownMicrophoneSource();
+            if(Application.isPlaying && this.enabled) { //If playing, actually open the mic source
                 TrySetupMicrophoneSource(value, (success)=>{
-                    Debug.Log("Microphone " + (value ?? "Default Device") + " " + (success ? "successfully opened" : "failed to open"));
                     if(success) {
                         _deviceName = value;
                     }
                 });
-                
             }
             else { //Otherwise, just change the property of the microphoneSource
                 _deviceName = value;
             }
         }
+    }
+
+    public event System.Action DeviceStateChanged;
+
+    public bool isListening {
+        get; private set;
     }
 
     ///<summary> Starts a microphone, no guarentee that it actually starts (see TrySetupMicrophoneSource) </summary>
@@ -49,45 +54,64 @@ public class MicrophoneAudioSource : MonoBehaviour {
         int minFreq, maxFreq;
         Microphone.GetDeviceCaps(deviceName, out minFreq, out maxFreq);
         int sampleFreq = maxFreq == 0 ? 44100 : maxFreq;
-        Debug.Log("Starting Microphone " + (deviceName ?? "Default Device") + " @ " + sampleFreq + ".");
+        Debug.Log("Starting Microphone " + (deviceName ?? "Default Device") + " @ " + sampleFreq + "Hz.");
         source.clip = Microphone.Start(deviceName, true, 10, sampleFreq);
         source.loop = true;
         source.Play();
     }
 
-    private IEnumerator TrySetupMicrophoneSourceCoroutine(string deviceName, System.Action<bool> cb = null) {
+    ///<summary>Coroutine that supports TrySetupMicrophoneSource, can't yield in a lambda :( </summary>
+    private IEnumerator TrySetupMicrophoneSourceCoroutine(string deviceName, System.Action<bool> cb) {
         SetupMicrophoneSource(deviceName);
         double startTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
         while(!(Microphone.GetPosition(deviceName) > 0)) {
             yield return null; //Let the coroutine continue
             double waitingTime = (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) - startTime;
             if(waitingTime > 1000) { //Waiting more than 1 second
-                if(cb != null) {
-                    cb(false);
-                }
+                isListening = false;
+                cb(false);
+                DeviceStateChanged();
                 yield break;
             }
         }
-        if(cb != null) {
-            cb(true);
-        }
+        isListening = true;
+        cb(true);
+        DeviceStateChanged();
     }
 
     ///<summary> Tries to start a MicrophoneSource but times out after 5 seconds, calling a callback </summary>
     ///<remarks> cb will be called when the microphone is successfully opened or times out after 1 second </remarks>
     public void TrySetupMicrophoneSource(string deviceName, System.Action<bool> cb = null) {
-        StartCoroutine(TrySetupMicrophoneSourceCoroutine(deviceName, cb));
+        TeardownMicrophoneSource();
+        StartCoroutine(TrySetupMicrophoneSourceCoroutine(deviceName, (success)=>{
+            Debug.Log($"Microphone {this._deviceName ?? "Default Device"} {(success ? "successfully listening" : "failed to listen")}");
+            if(cb != null) {
+                cb(success);
+            }
+        }));
     }
 
+    ///<summary>Detaches the microphone from the current audio source</summary>
     public void TeardownMicrophoneSource() {
+        if(!isListening) {
+            return;
+        }
         AudioSource source = GetComponent<AudioSource>();
         source.Stop();
         source.clip = null;
         Microphone.End(this.deviceName);
+        DeviceStateChanged();
     }
 
-    void Start() {
-        this.deviceName = null; //Trigger the microphone to setup for the first time
+    void Awake() {
+        isListening = false;
+    }
+
+    public void OnEnable() {
+        TrySetupMicrophoneSource(this._deviceName);
+    }
+    public void OnDisable() {
+        TeardownMicrophoneSource();
     }
 }
 
