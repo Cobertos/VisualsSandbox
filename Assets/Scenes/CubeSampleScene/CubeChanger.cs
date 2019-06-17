5f8908f5613faab8c151f9c1f4bd4b19add9263d
Peter / Cobertos <me+git@cobertos.com>
2019-06-17 18:51:49 -0400

    Moved all VisualsTools scripts into respective folder. Added CubeSampleScene

---
+++ Assets/Scenes/CubeSampleScene/CubeChanger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CobVisuals;
+
+namespace CobVisualsExamples {
+
+public class CubeChanger : MonoBehaviour {
+    public BPMSource bpmSource;
+    public System.Action changeFunc;
+    public System.Action changeFuncFixed;
+
+    // Start is called before the first frame update
+    void Start() {
+        changeFunc = bpmSource.onceEvery(1, 0.5, ()=>{
+            this.GetComponent<Renderer>().material.color = Mathf.Floor((float)bpmSource.beat % 2) == 0 ? Color.red : Color.green;
+        });
+        changeFuncFixed = bpmSource.onceEvery(1, 0, ()=>{
+            //Calculate a random position in the bounds of the object
+            Bounds meshBounds = this.GetComponent<MeshFilter>().sharedMesh.bounds;
+            Vector3 forcePosition = new Vector3(Random.value*2.0f-1.0f, Random.value*2.0f-1.0f, Random.value*2.0f-1.0f);
+            forcePosition.Scale(meshBounds.extents);
+            forcePosition += this.transform.position;
+            Vector3 forceDirection = new Vector3(Random.value*2.0f-1.0f, Random.value*2.0f-1.0f, Random.value*2.0f-1.0f);
+            forceDirection.Normalize();
+            forceDirection *= 4;
+            this.GetComponent<Rigidbody>().AddForceAtPosition(forceDirection, forcePosition, ForceMode.VelocityChange);//.
+        });
+    }
+
+    // Update is called once per frame
+    void Update() {
+        changeFunc();
+    }
+
+    void FixedUpdate() {
+        changeFuncFixed();
+        //Acceleration toward the origin
+        Vector3 vec2Origin = this.transform.position * -1;
+        vec2Origin.Normalize();
+        vec2Origin *= 40f;
+        this.GetComponent<Rigidbody>().AddForce(vec2Origin, ForceMode.Acceleration);
+    }
+}
+
+}
---
+++ Assets/Scenes/CubeSampleScene/CubeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CobVisuals;
+
+namespace CobVisualsExamples {
+
+public class CubeSpawner : MonoBehaviour {
+    public BPMSource bpmSource;
+    public GameObject spawnPrefab;
+    public System.Action spawnFunc;
+
+    // Start is called before the first frame update
+    void Start() {
+        spawnFunc = bpmSource.onceEvery(4, 0, ()=>{
+            GameObject go = Instantiate(spawnPrefab, Vector3.zero, Quaternion.identity);
+            go.GetComponent<CubeChanger>().bpmSource = bpmSource;
+        });
+    }
+
+    // Update is called once per frame
+    void Update() {
+        spawnFunc();
+    }
+}
+
+}
